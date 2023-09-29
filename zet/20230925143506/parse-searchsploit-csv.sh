2d20775a73e611e1ea99b4a8adf179669e0e3b09
Parallels <parallels@kali-linux-2022-2.localdomain>
2023-09-29 01:39:27 -0500

    remove problem file and embed it as a function

--- zet/20230925143506/parse-searchsploit-csv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# simple CSV parser for exploitdb searchsploit fuzzy finding
-while IFS="," read -r recordid filename title bad_date author exploittype platform
-do
-  echo "/usr/share/exploitdb/$filename:$title $author $platform $filename"
-done < <(tac /usr/share/exploitdb/files_exploits.csv | head -n -1)
-
